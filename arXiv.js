d521a05cf18b08b1508205ac6e292adda0114dbd
Hajime Fukuda <hajime.fukuda@ipmu.jp>
2016-03-25 00:45:52 +0900

    added comment feature

--- arXiv.js
+++ arXiv.js
@@ -1,11 +1,19 @@
 'use strict';
 
-var browser = chrome;
+let browser = chrome;
 
-var templateDir = 'templates/';
-var templateNames = ['comment', 'comment_box'];
-var templateExtension = '.templ';
-var templates = {};
+let templateDir = 'templates/';
+let templateNames = ['comment', 'comment_box'];
+let templateExtension = '.html';
+let templates = {};
+
+const AID_REGEXP = /abs\/([a-z\-\.]+\/[0-9]+|[0-9]{4}\.[0-9]{4,5})/;
+
+const BASE_URL = 'http://192.168.56.100:8000/';
+const LIST_COMMENT = 'list/';
+const SEND_COMMENT = 'comment/';
+const USER_INFO = 'user_info/';
+const LOGIN = 'accounts/login/'
 
 function _start() {
   // Add css
@@ -29,11 +37,61 @@ function _start() {
   Promise.all(promiseList).then(main);
 }
 
+function renewComments(aid, commentArea) {
+  $.get(BASE_URL + LIST_COMMENT + aid).then(
+    (comments) => {
+      for (let comment of comments) {
+        // maybe we can search inspire by orcid and set the profile page
+        let commentHTML = templates['comment'].render(comment);
+        commentArea.append(commentHTML);
+      }
+    },
+    () => {
+      // jump to the login page?
+    }
+  );
+}
+
 function main() {
   let title = $('h1.title')[0].innerText;
   let authors = $('div.authors')[0].innerText.split(',');
+  let aid = AID_REGEXP.exec(location.href)[1];
+  
   let commentBox = templates['comment_box'];
   $("div.endorsers").after(commentBox.render({title: title}));
+  
+  let commentTextArea = $('textarea#comment_text');
+  let commentArea = $('div#comments');
+  let commentForm = $('form#send_comment');
+  
+  commentForm.submit(() => {
+    
+    let comment = commentTextArea.val();
+    if (!comment) return false;
+    
+    commentTextArea.val('');
+    let data = {text: comment};
+    
+    $.ajax({
+      type: 'POST',
+      url: BASE_URL + SEND_COMMENT + aid,
+      contentType: 'application/json',
+      dataType : 'JSON',
+      data: JSON.stringify(data)
+    }).then(
+      () => {
+        commentArea.html('');
+        renewComments(aid, commentArea);
+      },
+      () =>{
+        // jump to the login page?
+      }
+    );
+    
+    return false;
+  });
+  
+  renewComments(aid, commentArea);
 }
 
 $(_start);
